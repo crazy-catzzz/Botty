cbb061072febdcfcf81860f2afbc309aa28b2be3
Davide Guerrazzi <g.davide2005@gmail.com>
2023-09-21 11:15:04 +0200

    Fixed types

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ import { BottyClient } from "./types/BottyClient.ts";
 import { event_handler_obj } from "./handlers/eventHandler.ts";
 
 // Bun environment variables
-const token : string = Bun.env.BOT_TOKEN;
+const token : string | undefined = Bun.env.BOT_TOKEN;
 
 // God I love typed languages
 const bot : BottyClient = new BottyClient(
