4bfc2cb0ff6dfd854263d52669356b0e74b92017
Davide Guerrazzi <g.davide2005@gmail.com>
2023-09-27 10:25:54 +0200

    Included event handler in BottyClient class

--- app.ts
+++ app.ts
@@ -1,8 +1,6 @@
 import { Events, GatewayIntentBits } from "discord.js";
 import { BottyClient } from "./types/BottyClient.ts";
 
-import { event_handler_obj } from "./handlers/eventHandler.ts";
-
 // Bun environment variables
 const token : string | undefined = Bun.env.BOT_TOKEN;
 
@@ -12,8 +10,5 @@ const bot : BottyClient = new BottyClient(
     intents: [GatewayIntentBits.Guilds]
   }
 );
-
-// Event handler
-event_handler_obj.init(bot);
 
 bot.login(token);
--- types/BottyClient.ts
+++ types/BottyClient.ts
@@ -1,13 +1,20 @@
 import { Client, ClientOptions } from "discord.js";
 import { cmd_handler_obj } from "../handlers/commandHandler.ts";
+import { event_handler_obj } from "../handlers/eventHandler.ts";
 
 export class BottyClient extends Client {
   cmd_handler : any;
+  event_handler : any;
 
   constructor(options : ClientOptions) {
-    super(options);
+    super(options); // Init Discord client
 
+    // Init command handler
     this.cmd_handler = cmd_handler_obj;
     this.cmd_handler.init();
+
+    // Init event handler
+    this.event_handler = event_handler_obj;
+    this.event_handler.init(this);
   }
 }
