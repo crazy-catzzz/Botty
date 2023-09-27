9f15a54f9b9195a7bb2b8462a6c8a3f32a00d382
Davide Guerrazzi <g.davide2005@gmail.com>
2023-09-27 10:49:02 +0200

    token in app.ts is now a string

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import { Events, GatewayIntentBits } from "discord.js";
 import { BottyClient } from "./types/BottyClient.ts";
 
 // Bun environment variables
-const token : string | undefined = Bun.env.BOT_TOKEN;
+const token : string = Bun.env.BOT_TOKEN!;
 
 // God I love typed languages
 const bot : BottyClient = new BottyClient(
